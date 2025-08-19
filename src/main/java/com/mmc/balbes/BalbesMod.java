674e6605d7e6784380c06e1487bc3087db31f0db
dof <crashgeometry7@gmail.com>
2025-08-19 15:00:26 +0300

    1

--- src/main/java/com/mmc/balbes/BalbesMod.java
+++ src/main/java/com/mmc/balbes/BalbesMod.java
@@ -1,4 +1,4 @@
-package com.mmc.test;
+package com.mmc.balbes;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,11 +29,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TestMod.MOD_ID)
-public class TestMod
+@Mod(BalbesMod.MOD_ID)
+public class BalbesMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "test";
+    public static final String MOD_ID = "balbes";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -60,7 +60,7 @@ public class TestMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public TestMod(FMLJavaModLoadingContext context)
+    public BalbesMod(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
